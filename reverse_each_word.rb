7eaed6bfc3d53c2a9e7cdae4d80b1e693d076556
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 22:58:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 def reverse_each_word(string)
   answer = ""
   array = string.split(" ")
-  array.each do |x|
+  array.each! do |x|
       x = x.reverse
   end
-  
+  return array.collect(" ")
 end
